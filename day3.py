a43be77400e982d8d291c0a4dd751980162271c0
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-20 22:55:07 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,115 @@
+# problem1 
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# problem2
+# i = 10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# problem3
+# i = 2 
+# while i <=20:
+#     print(i)
+#     i+=2
+
+# problem4
+# i = 1 
+# while i <=20:
+#     print(i)
+#     i+=2
+
+
+# problem5
+# i = 1
+# while i <= 5:
+#     print("*", end="")
+#     i+=1
+
+
+# sum of n natural no
+# n = int(input("Enter a no:"))
+# i =1
+# sum = 0
+# while (i<=n):
+#     sum+=i
+#     i+=1
+# print(sum)
+
+
+# multiplication
+# n = int(input("Enter a no :"))
+# i = 1
+# while (i<=10):
+#     print(f"{n} x {i}= {n*i}")
+#     i+=1
+    
+# count of digits
+# num=int(input("Enter the no digits:"))
+# count = 0
+# while num>0:
+#     num//=10
+#     count+=1
+# print(" The total digits are", count)
+
+
+# sum of the digits
+# n = int(input("Enter the digits"))
+# sum = 0
+# while n>0:
+#     sum +=n%10
+#     n = n//10
+# print(sum)
+
+
+
+# n = int(input("enter the digits:"))
+# sum = 0
+# while n>0:
+#     n=n//10
+#     sum+=1
+# print(sum)
+
+# n = int(input("enter the digits:"))
+# total = 0
+# while n>0:
+#     total+=n%10
+#     n=n//10
+# print(total)
+
+
+# reverse a no 
+# n = int(input("enter digits:"))
+# reverse_no = 0
+# while n!=0:
+#     digit = n%10
+#     reverse_no = (reverse_no*10) + digit
+#     n = n//10
+# print(reverse_no)
+
+# palindrome
+# n = (input("enter digits: "))
+# original = n 
+# reverse_no = 0
+# while n!=0:
+#     digit = n%10
+#     reverse_no = (reverse_no*10) + digit
+#     n = n//10
+     
+# if (original == reverse_no):
+#         print("It is palindrome")
+# else:
+#         print("not a palindrome")
+
+
+# factorial
+# n = int(input("enter a no: "))
+# i = 1
+# fact=1
+# while i<=n:
+#     fact=fact*i
+#     i+=1
+# print(fact)
+       
