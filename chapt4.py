8e8b9fff69ce93477a83fe29d42ea67e4b38c3c1
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-21 23:24:02 +0530

    Refactor and uncomment code in chapt4.py

--- chapt4.py
+++ chapt4.py
@@ -3,22 +3,22 @@
 # list = input("Enter 7 fruits names :")
 # print(list.split(","))
 
-'''list = [1,5,2,66,9,8,10,100]
-# list.sort()
-# list.reverse()
-# list.insert(5,50)
-# list.pop(5)
-# list.append(500)
-# list.remove(100)
-print(list)'''
+list = [1,5,2,66,9,8,10,100]
+list.sort()
+list.reverse()
+list.insert(5,50)
+list.pop(5)
+list.append(500)
+list.remove(100)
+print(list)
 
 # tuples 
-'''tuple =(1,"am",5,7 )
+tuple =(1,"am",5,7 )
 print(tuple)
 print(type(tuple))
 t=tuple.count("am")
 print(t)
-print(tuple.count("am"))'''
+print(tuple.count("am"))
 
 fruits =[]
 
@@ -38,7 +38,7 @@ f5 = input("Enter  fruits names :")
 fruits.append(f5)
 print(fruits)
 
-'''marks = []
+marks = []
 m1 = int(input("Enter marks for student 1: "))
 marks.append(m1)
 m2 = int(input("Enter marks for student 2: "))
@@ -50,11 +50,11 @@ marks.append(m4)
 m5 = int(input("Enter marks for student 5: "))
 marks.append(m5)
 marks.sort()
-print(marks)'''
+print(marks)
 
-# list sum
-# l1 = [5,6,7,8]
-# print(sum(l1))
+list sum
+l1 = [5,6,7,8]
+print(sum(l1))
 
 # tuple count
 a = (7,0,8,0,0,9)
