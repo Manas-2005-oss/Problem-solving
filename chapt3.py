8dbc91291513a2107e8d49b438440363393b49d7
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-21 23:21:21 +0530

    Refactor chapt3.py to include string operations

--- chapt3.py
+++ chapt3.py
@@ -3,17 +3,17 @@ name = input(" Enter your name:")
 print(f"Good morning {name}")
 
 # letter
-# letter = '''Dear <|NAME|>,
-# You are selected!
-# <|DATE|>'''
-# print(letter.replace("<|NAME|>","Manas").replace("<|DATE|>","19/05/2026"))'''
+letter = '''Dear <|NAME|>,
+You are selected!
+<|DATE|>'''
+print(letter.replace("<|NAME|>","Manas").replace("<|DATE|>","19/05/2026"))'''
 
 # detect double space 
-'''m = "Manas is  hired"
+m = "Manas is  hired"
 print(m.find("  "))
 
 # replace double sapce with single space 
-print(m.replace("  ", " "))'''
+print(m.replace("  ", " "))
 
 # special characters 
 letter = "Dear Manas,\nThis python course is nice.\nThanks!"
