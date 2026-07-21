3a179f471eb7a9fa7b09f719037f6d3d56c01455
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-21 23:14:01 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,52 @@
+# def greet():
+#     print("Welcome to dsa")
+# greet()
+# greet()
+# greet()
+
+# def stars():
+#     print("*****")
+# stars()
+# stars()
+
+# def greet():
+#     print("Hello, Manas")
+# greet()
+# greet()
+# greet()
+# greet()
+# greet()
+
+
+
+
+# def greet(name):
+#     print("Hello", name)
+# greet("Manas")
+# greet("L")
+# greet("V")
+
+# def square(n):
+#     print(n*n)
+# square(5)
+
+# def add(a,b):
+#     print(a+b)
+# add(2,6)
+# add(10,5)
+
+# return statement 
+# def add(a,b):
+#     return a + b
+# result = add(5,5)
+# print(result)
+
+# def cube(n):
+#     return n*n*n
+# x= cube(9)
+# print(x)
+def square(n):
+    return n * n
+    print("Done")
+
+print(square(4))
