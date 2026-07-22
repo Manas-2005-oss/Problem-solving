0e5b035d8cac291fe997a83bfd07bef95d387df2
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-22 22:29:06 +0530

    Refactor chapt5.py to improve code structure

--- chapt5.py
+++ chapt5.py
@@ -1,19 +1,18 @@
-"""d={
+d={
     "anand": "happy",
     "ajja": "come",
     2: "two"
 }
 dict=(input("Enter the key: "))
-print(d[dict])"""
+print(d[dict])
 
 # set
-"""
 s = {1,5,7,8,9,2,6,10}
 print(s)
 
 set = set(input("Enter the numbers: "))
-print(set)"""
-"""
+print(set)
+
 s= set()
 n = int(input("Enter the number of elements: "))
 s.add(n)
@@ -31,14 +30,13 @@ n = int(input("Enter the number of elements: "))
 s.add(n)
 n = int(input("Enter the number of elements: "))
 s.add(n)
-print(s)"""
+print(s)
 
-'''
 s = set()
 s.add(18)
 s.add("18")
-print(s)'''
-"""
+print(s)
+
 d={}
 name=input("Enter the name: ")
 lang = input("Enter the language: ")
