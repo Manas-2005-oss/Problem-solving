7468a844207e1eb2a8ec4b6fe650c35404ba35a1
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-07-25 23:50:36 +0530

    code for problems in day1.py

--- day1.py
+++ day1.py
@@ -5,13 +5,13 @@
 # print(name,age,branch)
 
 # # problem2
-# a = 2
-# b = 5
+a = 2
+b = 5
 
-# temp = a
-# a = b
-# b = temp
-# print(a, b)
+temp = a
+a = b
+b = temp
+print(a, b)
 
 # problem3
 # a = 5
