44d2ac93628cc71cec47aaf34ebe0319bce44687
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-05 15:03:44 +0530

    updated code chapt9.py

--- chapt9.py
+++ chapt9.py
@@ -1,10 +1,10 @@
 # 'File I/O'
 
-# # Open a file for reading
-# # f= open("file.txt")
-# # b=f.read()
-# # print(b)
-# # f.close()
+Open a file for reading
+f= open("file.txt")
+b=f.read()
+print(b)
+f.close()
 
 # # Open file for Reading
 # b = "Manas Ippalpalli is Senior Developer who earning with package in crores and happily enjoying is life with his family and friends"
