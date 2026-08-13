4469a4c38172a722f61bc9d459f83f10ad81b73d
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-13 23:30:27 +0530

    Restore and uncomment problems in day2.py

--- day2.py
+++ day2.py
@@ -80,11 +80,11 @@ elif(a==c and a<b):
     print("a and c are equal and lesser than b")
 
 # # problem8
-# year = int(input("Enter a year:"))
-# if year % 4 == 0 and year % 100 !=0 or year % 400 == 0 :
-#     print("It is a leap year")
-# else:
-#     print("It is not a leap year")
+year = int(input("Enter a year:"))
+if year % 4 == 0 and year % 100 !=0 or year % 400 == 0 :
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
 
 # # problem9
 # word = (input("Enter a letter:"))
