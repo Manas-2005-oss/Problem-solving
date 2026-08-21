6231391ef92ba532ea592269eea80fb4ede2e8f7
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-21 22:53:38 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -5,8 +5,8 @@ for i in range(1,11):
 for i in range(2,21,2):
     print(i)
 
-# for i in range(1,21,2):
-#     print(i)
+for i in range(1,21,2):
+    print(i)
 
 # for i in range(10,0,-1):
 #     print(i)
