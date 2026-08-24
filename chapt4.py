63fd0801cf8ef9c8283011dc0625e6bba0adaa2b
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-24 22:55:20 +0530

    Refactor chapt4.py to improve readability

--- chapt4.py
+++ chapt4.py
@@ -13,12 +13,12 @@ list.remove(100)
 print(list)
 
 # tuples 
-'''tuple =(1,"am",5,7 )
+tuple =(1,"am",5,7 )
 print(tuple)
 print(type(tuple))
 t=tuple.count("am")
 print(t)
-print(tuple.count("am"))'''
+print(tuple.count("am"))
 
 fruits =[]
 
