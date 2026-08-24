6a21b12d6d7f37b669af431017d47f37ab165461
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-24 22:51:09 +0530

    Fix print statement for comparison of x and y

--- chapt2.py
+++ chapt2.py
@@ -13,10 +13,10 @@ print("the value of x is ", x)
 print("the type of x is ", type(x)) 
 
 # pg4
-"""
+
 x = int(input("enter the value of x: "))
 y = int(input("enter the value of y: "))
-print (  x>y)"""
+print (  x>y)
 
 #pg5
 """
