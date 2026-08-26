e45cd7ccb9ad0253acf8e224cba6591e660d1237
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-26 21:57:58 +0530

    code in chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -14,9 +14,9 @@ while (i < len(l1)):
     print(l1[i])
     i += 1
 
-# l1 =(1,2,"manas","manas","lokesh","sai","rohit",8,9)
-# for item in l1:
-#     print(item)
+l1 =(1,2,"manas","manas","lokesh","sai","rohit",8,9)
+for item in l1:
+    print(item)
 
 # break statement
 # for i in range(1,50,2):
