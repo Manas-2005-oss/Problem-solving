cb6149c55b82aaf05e533b94d3748302cc20c367
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-26 21:56:43 +0530

    Update in chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -4,8 +4,8 @@ while (i<51):
     print(i)
     i += 1 
 
-# for i in range(1,51):
-#     print(i)
+for i in range(1,51):
+    print(i)
 # #  list printing
 # 
 # l1 =[1,2,"manas","manas","lokesh","sai","rohit",8,9]
