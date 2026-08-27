8a47b53bae01ef776f8c0ebed2c667649d4ff8f0
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-27 22:50:32 +0530

    Loops and input handling in chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -62,11 +62,11 @@ while (i<=n):
 print(sum)
 
 # factorial
-# n= int(input("Enter the number: "))
-# fact = 1
-# for i in range (1,n+1):
-#     fact = fact *i
-# print(f"Factorial of {n} is {fact}")
+n= int(input("Enter the number: "))
+fact = 1
+for i in range (1,n+1):
+    fact = fact *i
+print(f"Factorial of {n} is {fact}")
 # n = int(input("Enter the number: "))
 # fact = 1
 # i=1
