acaa5fbf2a7f466a9ab94b865f028c17c2ab36e0
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-28 22:46:29 +0530

    Reformat chapt3.py with consistent spacing

--- chapt3.py
+++ chapt3.py
@@ -8,6 +8,8 @@ You are selected!
 <|DATE|>'''
 print(letter.replace("<|NAME|>","Manas").replace("<|DATE|>","19/05/2026"))
 
+
+
 # detect double space 
 '''m = "Manas is  hired"
 print(m.find("  "))
