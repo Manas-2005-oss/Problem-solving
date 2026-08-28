fa06ef63a5746e894d97b5e09932dea529966207
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-28 22:23:25 +0530

    update

--- Project2.py
+++ Project2.py
@@ -3,14 +3,12 @@ def name():
     name = input("Enter students name:")
     print("Student name is:", name)
     return name
-
 def get_marks():
     maths= float(input("Enter marks in maths:"))
     history= float(input("Enter marks in history:"))
     marathi= float(input("Enter marks in marathi:"))
     hindi= float(input("Enter marks in hindi:"))
     return maths,history,marathi,hindi
-
 def calculate_percentage(maths,history,marathi,hindi):
     total_percentage = (maths+history+marathi+hindi)/400 *100
     print("Total Percentage is:", total_percentage)
@@ -20,7 +18,6 @@ def calculate_avg(maths,history,marathi,hindi):
     avg = (maths+history+marathi+hindi)/4
     print("average marks is:",avg)
     return avg
-
 def calculate_grade(total_percentage):
     if total_percentage >= 90:
         print("The grade is A")
@@ -32,8 +29,6 @@ def calculate_grade(total_percentage):
         print("The grade is D")
     else:
         print("The grade is F")
-
-
 name = name()
 marks = get_marks()
 percentage = calculate_percentage(marks[0], marks[1], marks[2], marks[3])
