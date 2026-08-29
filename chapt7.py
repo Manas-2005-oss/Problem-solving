2c33c07790894e0d6adbf26ba7dceb6bcfe5890a
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-29 23:03:19 +0530

    Update chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -89,15 +89,15 @@ for i in range (5,0,-1):
 * *
 ***
 '''
-# n=3
-# for i in range (1,n+1):
-#    if i==1 or i==n:
-#     print("*"*n)
-#    else:
-#      print("*", end="")
-#      print(" "*(n-2), end="" )
-#      print("*", end="")
-#    print("")
+n=3
+for i in range (1,n+1):
+   if i==1 or i==n:
+    print("*"*n)
+   else:
+     print("*", end="")
+     print(" "*(n-2), end="" )
+     print("*", end="")
+   print("")
 
 # n = int(input("Enter the number: "))
 # for i in range (1,11):
