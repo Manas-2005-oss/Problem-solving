cc1d001c1457d85ef0ce68da3701a6e9ec9e6fcd
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-29 23:04:10 +0530

    Update chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -99,6 +99,6 @@ for i in range (1,n+1):
      print("*", end="")
    print("")
 
-# n = int(input("Enter the number: "))
-# for i in range (1,11):
-#     print(f"{n} x {11-i} = {n*(11-i)}") 
+n = int(input("Enter the number: "))
+for i in range (1,11):
+    print(f"{n} x {11-i} = {n*(11-i)}") 
