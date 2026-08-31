1f842a466ab5697c4afab9fe9dea34c8c08998ac
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-08-31 22:02:16 +0530

    Re-add code snippets for various calculations

--- chatp2.py
+++ chatp2.py
@@ -7,10 +7,10 @@ print(x+y)
 print(" the remainder of x divide by y is ", x%y)
 
 #pg3
-"""
+
 x = input("enter the value of x: ")
 print("the value of x is ", x)
-print("the type of x is ", type(x)) """
+print("the type of x is ", type(x)) 
 
 # pg4
 """
