8dd38d21ea4133e523562aab5c8975c092d67c43
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-02 22:23:18 +0530

    Fix input handling and comparison in chapt2.py

--- chapt2.py
+++ chapt2.py
@@ -10,7 +10,7 @@ print(" the remainder of x divide by y is ", x%y)
 x = input("enter the value of x: ")
 print("the value of x is ", x)
 print("the type of x is ", type(x)) 
-
+               
 # pg4
 x = int(input("enter the value of x: "))
 y = int(input("enter the value of y: "))
