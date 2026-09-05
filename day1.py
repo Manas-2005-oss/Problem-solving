a1bffc4f86080f1f884bf3bf29f5cb7131fa6d5e
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-05 22:56:51 +0530

    Problems 1 and 2 in day1.py

--- day1.py
+++ day1.py
@@ -4,14 +4,14 @@ age = 21
 branch = "CSM"
 print(name,age,branch)
 
-# # problem2
-# a = 2
-# b = 5
+# problem2
+a = 2
+b = 5
 
-# temp = a
-# a = b
-# b = temp
-# print(a, b)
+temp = a
+a = b
+b = temp
+print(a, b)
 
 # problem3
 # a = 5
