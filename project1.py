fd025350891cf8dce4a61f39662450ad05c0cbe1
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-06 20:43:54 +0530

    Refactor snake water gun game code

--- project1.py
+++ project1.py
@@ -10,8 +10,7 @@ dict = {"s":1,"w":-1,"g":0}
 reverse_dict ={1:"snake",-1:"water",0:"gun"}
 user =dict[user]
 
-print(f"Computer choice is {reverse_dict[computer]}\n and user choice is {reverse_dict[user]}")
-      
+print(f"Computer choice is {reverse_dict[computer]}\n and user choice is {reverse_dict[user]}")     
 if computer == user:
     print("Its a tie!")
 else:
