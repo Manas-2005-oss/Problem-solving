3806baf8349d067b17919e911313a954804218b3
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-06 20:42:57 +0530

    Fix formatting and syntax issues in snake game

--- project1.py
+++ project1.py
@@ -11,7 +11,7 @@ reverse_dict ={1:"snake",-1:"water",0:"gun"}
 user =dict[user]
 
 print(f"Computer choice is {reverse_dict[computer]}\n and user choice is {reverse_dict[user]}")
-
+      
 if computer == user:
     print("Its a tie!")
 else:
