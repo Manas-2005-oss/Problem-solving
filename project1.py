3df0b4ea00dd1f42ba7bbe8f1753a77705ebd72c
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-06 20:42:30 +0530

    Refactor snake water gun game code

--- project1.py
+++ project1.py
@@ -1,11 +1,9 @@
 # snake water gun game
 import random
-
 '''
 1=snake
 -1 water
 0 gun'''
-
 computer = random.choice([1,-1,0])
 user = input("Enter your choice(s,w,g): ")
 dict = {"s":1,"w":-1,"g":0}
