603ebdc85824d1edd6b462010d32f213b2aaaf89
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-10 22:14:57 +0530

    Refactor loops and input handling in chapt7.py

--- chapt7.py
+++ chapt7.py
@@ -103,6 +103,7 @@ for i in range (1,n+1):
      print("*", end="")
    print("")
 
+# printing multiplication table
 n = int(input("Enter the number: "))
 for i in range (1,11):
     print(f"{n} x {11-i} = {n*(11-i)}") 
