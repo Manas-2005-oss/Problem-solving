42342a248a501f84a929fe2853f6feee3aa9d6f3
Manas-2005-oss <manasippalpalli@gmail.com>
2026-09-11 21:42:35 +0530

    finding target in array using pair sum and two pointer pattern

---
+++ finding_target_in_array.py
@@ -0,0 +1,13 @@
+arr = [2,7,11,15]
+target = 9
+left=0
+right=len(arr)-1
+while left<right:
+    total=arr[left]+arr[right]
+    if total==target:
+        print(total,"found it",arr[left],arr[right])
+        break
+    elif total>target:
+        right-=1
+    elif total<target:
+        left+=1
