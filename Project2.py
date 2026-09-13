25aa05677001c32169a8786efb9149a5659161ab
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-13 21:49:45 +0530

    Refactor student marks calculator code structure

--- Project2.py
+++ Project2.py
@@ -9,7 +9,7 @@ def get_marks():
     marathi= float(input("Enter marks in marathi:"))
     hindi= float(input("Enter marks in hindi:"))
     return maths,history,marathi,hindi
-def calculate_percentage(maths,history,marathi,hindi):
+def calculate_percentage(maths,history,marathi,hindi): 
     total_percentage = (maths+history+marathi+hindi)/400 *100
     print("Total Percentage is:", total_percentage)
     return total_percentage
