ced9b1d6210e2e9b2e614a2f335ae9ed2c287643
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-13 21:50:49 +0530

    Refactor student marks calculator functions

--- Project2.py
+++ Project2.py
@@ -1,6 +1,6 @@
 # simple students marks calculator using functions and lists       
 def name():
-    name = input("Enter students name:")
+    name = input("Enter students name:") 
     print("Student name is:", name)  
     return name                     
 def get_marks():
