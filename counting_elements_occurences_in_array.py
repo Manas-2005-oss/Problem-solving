b67b84aef58ae24a092ff56c8b1d7fcb684b0482
Manas Ippalpalli <manasippalpalli@gmail.com>
2026-09-17 22:15:22 +0530

    Enhance counting of multiple elements in array
    Added functionality to count occurrences of multiple target elements in the array.

--- counting_elements_occurences_in_array.py
+++ counting_elements_occurences_in_array.py
@@ -6,3 +6,18 @@ for num in arr:
     if num == target:
         count+=1
 print(count)
+
+# counting repearted multiple elements in array
+arr = [5, 2, 5, 8, 5, 9, 3, 9]
+target1 = 5
+target2= 9
+count1 = 0
+count2 = 0
+for num in arr:
+    if num == target1:
+        count1+=1
+    if num== target2:
+        count2+=1
+print(count1)
+print(count2)
+
